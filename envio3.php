aca8dc1aec3c6bd607ff9bc562445b7c0dfb7006
Petter De la Cruz <66736889+petdela@users.noreply.github.com>
2021-08-25 00:07:41 -0500

    Update envio3.php

--- envio3.php
+++ envio3.php
@@ -39,7 +39,7 @@
         <body> 
         <h1>Hola amigos!</h1> 
         <p> 
-        <b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
+        <b>Bienvenidos a mi correo electrónico de prueba</b>.  
         </p> 
         </body> 
         </html> 
